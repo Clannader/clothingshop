85a016e32641691308bbc127a5e0bfd49175112e
Clannader <oliver..wu@shijigroup.com>
2020-02-23 00:30:16 +0800

    更新Handsontable

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,22 +2,27 @@
   <div>
     <v-card class="card-title">
       <v-card-title class="headline">Vuetify Table</v-card-title>
+      <v-divider></v-divider>
       <v-data-table
         :headers="vueHeaders"
-        :items="desserts"
+        :items="vueDesserts"
         disable-sort
         :page.sync="page"
-        :items-per-page="itemsPerPage"
+        :items-per-page="Number(itemsPerPage)"
         hide-default-footer
-        class="elevation-1"
+        class="mr-24"
+        show-select
+        fixed-header
+        item-key="register"
+        :mobile-breakpoint="0"
         @page-count="pageCount = $event"
       ></v-data-table>
-      <v-layout row class="pt-2">
-        <v-flex class="flexCenter">
-          <span class="amount">总数: {{count}}</span>
-        </v-flex>
-        <v-layout row>
-          <v-flex class="flexCenter flexEnd">
+      <v-row class="mr-24">
+        <v-col class="flex-center">
+          <span class="amount">总数: {{vueDesserts.length}}</span>
+        </v-col>
+        <v-row class="mr-24">
+          <v-col class="flex-center flexEnd">
             <span class="everyPageShow">每页总数</span>
             <div class="showNumberBox">
               <v-select
@@ -31,20 +36,30 @@
               v-model="page"
               :length="pageCount"
               :total-visible="7"
+              style="padding-left: 20px"
             ></v-pagination>
-          </v-flex>
-        </v-layout>
-      </v-layout>
+          </v-col>
+        </v-row>
+      </v-row>
     </v-card>
     <v-card class="card-title">
       <v-card-title class="headline">Handson Table</v-card-title>
-      <hot-table>
+      <v-divider></v-divider>
+      <hot-table
         ref="hotTable"
-        :settings="hotTableSettings"
+        :settings="hotTableSettings">
+      </hot-table>
+    </v-card>
+    <v-card class="card-title">
+      <v-card-title class="headline">Handson Table</v-card-title>
+      <v-divider></v-divider>
+      <hot-table
+        ref="handsonTable">
       </hot-table>
     </v-card>
     <v-card class="card-title">
       <v-card-title class="headline">Ant Design Table</v-card-title>
+      <v-divider></v-divider>
     </v-card>
   </div>
 </template>
@@ -77,33 +92,93 @@
         }
         body.push(data)
       }
-      const vueHeaders = []
-      headers.forEach(h => {
-        vueHeaders.push({
-          text: h,
-          width: '120',
-          value: 'name'
-        })
-      })
-      const desserts = []
-      for (let i = 0; i < count; i++) {
-        desserts.push({
-          name: 'ASSS(' + i + ')'
-        })
-      }
+
       return {
-        vueHeaders: vueHeaders,
-        desserts: desserts,
+        vueHeaders: this.getVueHeaders(),
+        vueDesserts: this.getVueDesserts(),
         page: 1,
-        pageCount: 5,
-        itemsPerPage: 2,
-        count: count,
-        showNumber: ['2', '4', '6', '10'],
+        pageCount: 10,
+        itemsPerPage: 10,
+        showNumber: ['10', '20', '40', '100'],
         hotTableSettings: {
           headers: ['1', '2', '3'],
           rowHeaders: ['A', 'B', 'C'],
           colHeaders: ['D', 'F', 'W'],
           data: [[2, 3, 4], [4, 6, 7], [12, 32, 43]]
+        }
+      }
+    },
+    methods: {
+      getVueHeaders() {
+        const vueHeaders = []
+        vueHeaders.push({
+          text: '预订号',
+          value: 'register'
+        })
+        vueHeaders.push({
+          text: '房间号',
+          width: 100,
+          value: 'roomNumber'
+        })
+        vueHeaders.push({
+          text: '房间数',
+          width: 100,
+          value: 'rooms'
+        })
+        vueHeaders.push({
+          text: '客人名',
+          value: 'guestName'
+        })
+        vueHeaders.push({
+          text: '到店日期',
+          width: 120,
+          value: 'arrivalDate'
+        })
+        vueHeaders.push({
+          text: '离店日期',
+          width: 120,
+          value: 'departureDate'
+        })
+        vueHeaders.push({
+          text: '档案号',
+          value: 'profileNumber'
+        })
+        vueHeaders.push({
+          text: '预订状态',
+          value: 'status'
+        })
+        vueHeaders.push({
+          text: '房价',
+          value: 'roomPrice'
+        })
+        vueHeaders.push({
+          text: '房价码',
+          width: 100,
+          value: 'rateCode'
+        })
+        return vueHeaders
+      },
+      getVueDesserts() {
+        const vueDesserts = []
+        const num = 300
+        for (let i = 0; i < num; i++) {
+          vueDesserts.push(this.getVueTempData(i))
+        }
+        return vueDesserts
+      },
+      getVueTempData(num) {
+        const arr = moment().add(num, 'days')
+        return {
+          register: '123456' + num,
+          roomNumber: '00' + (num + 1),
+          rooms: (num % 2 === 0 ? 1 : 2),
+          guestName: 'Rabbit' + num,
+          arrivalDate: arr.format('YYYY-MM-DD'),
+          departureDate: arr.add(1, 'days').format('YYYY-MM-DD'),
+          profileNumber: '325341' + num * 2,
+          status: 'CheckIn',
+          roomPrice: '780',
+          rateCode: 'DFG'
         }
       }
     }
@@ -123,7 +198,16 @@
     display: flex;
   }
 
-  .flexCenter {
+  .mr-24{
+    margin-left: 0px;
+    margin-right: 0px;
+  }
+
+  .v-pagination{
+    width: auto;
+  }
+
+  .flex-center {
     display: flex;
     align-items: center;
   }
@@ -148,6 +232,19 @@
 
   .showNumberBox {
     display: inline-block;
-    width: 60px;
+    width: 40px;
+  }
+
+  /deep/ .v-select__slot{
+    max-width: none;
+    width: auto;
+  }
+
+  /deep/ .v-select__selection--comma{
+    overflow: visible;
+  }
+
+  /deep/ .v-data-table__wrapper{
+    max-height: 300px;
   }
 </style>
