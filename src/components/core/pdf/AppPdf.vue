504acfe1447746ee15cc4e438bfd21baadaf51ca
Clannader <oliver..wu@shijigroup.com>
2020-05-19 23:04:36 +0800

    修改代码

--- src/components/core/pdf/AppPdf.vue
+++ src/components/core/pdf/AppPdf.vue
@@ -50,7 +50,7 @@
     watch: {
       pageIndex() {
         // 当每次分页的时候,加载对应页码的pdf
-        this.pageCanvas(this.pageIndex)
+        this.pageCanvas()
         // 由于有bug,所以就只能通过这个方法回到顶部
         this.$vuetify.goTo('.pdf-list', {
           duration: 500,
@@ -59,47 +59,50 @@
         })
       },
       pdfContent: {
-        handler(val) {
+        async handler(val) {
           if (val) {
             // 当检测内容有变化的时候,需要跳回第一页的操作
             // 重新加载pdf
-            this.showPdf(val)
+            await this.showPdf(val, this.pageIndex !== 1)
             // 返回顶部
+            // 如果页数等于1,不会触发watch
+            this.pageIndex = 1
             this.$vuetify.goTo('.pdf-list', {
               duration: 500,
               easing: 'linear',
               container: '.pdf-list'
             })
-            // this.pageIndex = 1
           }
         }
       }
     },
     mounted() {
       // 初始化时加载pdf
-      this.showPdf(this.pdfContent)
+      this.showPdf(this.pdfContent, false)
     },
     methods: {
-      async showPdf(base64) {
+      async showPdf(base64, isChange) {
         // const pdfList = document.querySelector('.pdfList') // 通过querySelector选择DOM节点,使用document.getElementById()也一样
         // const CMAP_URL = 'https://unpkg.com/pdfjs-dist@2.3.200/cmaps/'
         const decodedBase64 = atob(base64) // 使用浏览器自带的方法解码
         PDFJS.GlobalWorkerOptions.workerSrc = PDFJS
         // 返回一个pdf对象
-        // TODO 这里有个bug,当切换内容的时候,getPage报错,并且第一页会颠倒显示
         this.pdfObject = await PDFJS.getDocument({
           data: decodedBase64,
           // cMapUrl: CMAP_URL,
           cMapPacked: true
         })
         this.total = this.pdfObject.numPages // 声明一个pages变量等于当前pdf文件的页数
-        this.pageCanvas(1)
+        // 这里避免pdf内容改变时,切换至第一页的时候,修改页数不能走这里触发页数渲染
+        if (!isChange) {
+          this.pageCanvas()
+        }
       },
-      async pageCanvas(pageIndex) {
+      async pageCanvas() {
         // const pdfList = document.querySelector('.pdf-list')
         // const canvas = document.createElement('canvas')
         const canvas = this.$refs.pdfCanvas
-        const page = await this.pdfObject.getPage(pageIndex) // 调用getPage方法传入当前循环的页数,返回一个page对象
+        const page = await this.pdfObject.getPage(this.pageIndex) // 调用getPage方法传入当前循环的页数,返回一个page对象
         const scale = 1.5 // 缩放倍数，1表示原始大小
         const viewport = page.getViewport(scale)
         const context = canvas.getContext('2d') // 创建绘制canvas的对象
