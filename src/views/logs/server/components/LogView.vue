d49d1d2107134cda832beb03ba2dad9884f30f7f
Clannader <oliver..wu@shijigroup.com>
2020-11-10 20:30:11 +0800

    完善服务器日志

--- src/views/logs/server/components/LogView.vue
+++ src/views/logs/server/components/LogView.vue
@@ -24,6 +24,7 @@
           :height="tableY"
           :items="logContent"
           item-height="25"
+          v-scroll.self="onScroll"
         >
           <template v-slot:default="{ item }">
             <div>
@@ -65,7 +66,7 @@
         // logTemp: '', // 这个是分隔数组的最后一行内容
         tableY: 230,
         startByte: 0, // 开始加载的字节位数
-        endByte: 1 * 1024 // 最大加载1MB
+        endByte: 10 * 1024 // 最大加载1MB
       }
     },
     created() {
@@ -94,6 +95,7 @@
           // }
           // 测试用例11-09 10-17
           // const content = Buffer.from(res.content, 'base64').toString()
+          this.hasMore = res.hasMore
           const arrContent = Buffer.from(res.content, 'base64').toString().split('\r\n')
           // 取得到数组中的数组的最后一个元素
           const lastArr = arrContent[arrContent.length - 1]
@@ -102,6 +104,10 @@
           if (lastArr === '') {
             // 干掉最后一个元素
             arrContent.splice(arrContent.length - 1, 1)
+            if (!this.hasMore) {
+              // 判断是不是到最末尾了
+              arrContent[0] = this.logContent.splice(this.logContent.length - 1, 1) + arrContent[0]
+            }
           } else {
             // 改变第一个元素的值
             arrContent[0] = this.logContent.splice(this.logContent.length - 1, 1) + arrContent[0]
@@ -110,7 +116,6 @@
           }
           // this.logTemp = lastArr
           this.logContent = this.logContent.concat(arrContent)
-          this.hasMore = res.hasMore
           if (this.hasMore) {
             this.startByte = res.startByte
             this.endByte = res.endByte
@@ -125,6 +130,17 @@
       },
       onResize() {
         this.tableY = window.innerHeight - 180
+      },
+      onScroll(e) {
+        // 2个标识,阻止多次请求导致加载重复数据
+        // 如果没有数据了,也不需要加载
+        // 如果正在加载中,也不需要加载
+        if (!this.hasMore || this.loading) return
+        // 这里不能使用相等,因为不是100%用户会滚到最底部的
+        if (e.target.scrollTop + e.target.offsetHeight > e.target.scrollHeight - 100) {
+          // 监控到底部了
+          this.loadingLog()
+        }
       }
     }
   }
