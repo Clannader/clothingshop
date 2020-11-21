26166610dbfd332293ebf5afde9abeaccc6f8b23
Clannader <oliver..wu@shijigroup.com>
2020-11-21 20:09:54 +0800

    优化代码

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,7 +6,7 @@ Vue.use(Vuetify)
 
 const opts = {
   breakpoint: {
-    mobileBreakpoint: 960
+    mobileBreakpoint: 960 // 当窗口小于960px时,系统就认为是手机模式
   },
   icons: {
     iconfont: ['mdiSvg'] // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4'
