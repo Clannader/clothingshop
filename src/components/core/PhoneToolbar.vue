522cb77962d60ca07611a8b21f41245bdd127190
Clannader <oliver..wu@shijigroup.com>
2020-12-08 18:58:12 +0800

    更新手机端路由代码

--- src/components/core/PhoneToolbar.vue
+++ src/components/core/PhoneToolbar.vue
@@ -8,29 +8,36 @@
     :src="require('@/assets/images/vbanner.png')"
     color="primary"
   >
-    <v-app-bar-nav-icon
-    >
+    <v-app-bar-nav-icon @click="goback()">
+      <v-icon>
+        arrow_back
+      </v-icon>
     </v-app-bar-nav-icon>
-
+    <v-toolbar-title>{{$t(meta.title)}}</v-toolbar-title>
     <v-spacer></v-spacer>
 
-    <v-btn
-      icon
-    >
-      <v-icon>
-        mdi-apps
-      </v-icon>
-    </v-btn>
-
+    <slot name="icon"></slot>
   </v-app-bar>
 </template>
 
 <script>
+  import { get } from 'vuex-pathify'
+
   export default {
-    name: 'PhoneToolbar'
+    name: 'PhoneToolbar',
+    computed: {
+      ...get('tagsView', ['currentRouter@meta'])
+    },
+    methods: {
+      goback() {
+        this.$router.back(-1)
+      }
+    }
   }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .v-toolbar__title{
+    font-size: 1rem;
+  }
 </style>
--- src/phone/settings/user/index.vue
+++ src/phone/settings/user/index.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="v-application--wrap">
-    <phone-toolbar></phone-toolbar>
+    <phone-toolbar>
+
+      <template v-slot:icon>
+        <v-btn
+          icon
+        >
+          <v-icon>
+            refresh
+          </v-icon>
+        </v-btn>
+      </template>
+
+      <template v-slot:extension>
+        <app-text-icon
+          :text="new Date().format('YYYY年MM月DD日')"
+          style="margin-top: 2px"
+          icon="mdi-calendar-blank"
+        ></app-text-icon>
+      </template>
+    </phone-toolbar>
   </div>
 </template>
 
