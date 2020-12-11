b142afa3e5b855a1fe0e3ea7223a3117e1b5b7ab
Clannader <oliver..wu@shijigroup.com>
2020-12-11 18:11:35 +0800

    提交代码

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -3,16 +3,16 @@
  */
 'use strict'
 
-import { CellGroup, Button, Cell, SwipeCell, Tabbar, TabbarItem, Icon } from 'vant'
+// import { CellGroup, Button, Cell, SwipeCell, Tabbar, TabbarItem, Icon } from 'vant'
 // import 'vant/lib/index.css'
 // import '@vant/touch-emulator' // PC端不能加入这个,否则导致有问题
 
 export function useVant(app) {
-  app.component(CellGroup.name, CellGroup)
-  app.component(Button.name, Button)
-  app.component(Cell.name, Cell)
-  app.component(SwipeCell.name, SwipeCell)
-  app.component(Tabbar.name, Tabbar)
-  app.component(TabbarItem.name, TabbarItem)
-  app.component(Icon.name, Icon)
+  // app.component(CellGroup.name, CellGroup)
+  // app.component(Button.name, Button)
+  // app.component(Cell.name, Cell)
+  // app.component(SwipeCell.name, SwipeCell)
+  // app.component(Tabbar.name, Tabbar)
+  // app.component(TabbarItem.name, TabbarItem)
+  // app.component(Icon.name, Icon)
 }
